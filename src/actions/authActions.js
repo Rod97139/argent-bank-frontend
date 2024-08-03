64cc54d42921545b0fadcb3a93c97396725a5205
Rod97139 <fayaflame@gmail.com>
2024-08-03 22:40:21 +0200

    move axios requests to services folder

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,75 +1,12 @@
-import axios from 'axios'
 import { createAsyncThunk } from '@reduxjs/toolkit'
-
-const backendURL = 'http://localhost:3001/api/v1'
-
-// export const registerUser = createAsyncThunk(
-//   'auth/register',
-//   async ({ email, password }, { rejectWithValue }) => {
-//     try {
-//       const config = {
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       }
-//       await axios.post(
-//         `${backendURL}/user/login`,
-//         { email, password },
-//         config
-//       )
-//     } catch (error) {
-//     // return custom error message from backend if present
-//       if (error.response && error.response.data.message) {
-//         return rejectWithValue(error.response.data.message)
-//       } else {
-//         return rejectWithValue(error.message)
-//       }
-//     }
-//   }
-// )
-
-// export const userProfile = createAsyncThunk(
-//   'auth/profile',
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       const config = {
-//         headers: {
-//           'Content-Type': 'application/json',
-//           Authorization: `Bearer ${localStorage.getItem('userToken')}`,
-//         },
-//       }
-//       const { data } = await axios.get(`${backendURL}/user/profile`, config)
-//       return data
-//     } catch (error) {
-//       if (error.response && error.response.data.message) {
-//         return rejectWithValue(error.response.data.message)
-//       } else {
-//         return rejectWithValue(error.message)
-//       }
-//     }
-//   }
-// )
+import { userLoginRequest, userUpdateRequest } from '../service/authService'
 
 export const userUpdate = createAsyncThunk(
   'auth/update',
   async ({ firstName, lastName }, { rejectWithValue, getState }) => {
     try {
-      
       const token = getState().auth.userToken
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      }
-      const { data } = await axios.put(
-        `${backendURL}/user/profile`,
-        {
-          "firstName": firstName,
-          "lastName": lastName
-        },
-        config
-      )
+      const { data } = await userUpdateRequest(firstName, lastName, token)
       return data
     } catch (error) {
       if (error.response && error.response.data.message) {
@@ -85,17 +22,7 @@ export const userLogin = createAsyncThunk(
   'auth/login',
   async ({ email, password, rememberMe }, { rejectWithValue }) => {
     try {
-      // configure header's Content-Type as JSON
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-      const { data } = await axios.post(
-        `${backendURL}/user/login`,
-        { email, password },
-        config
-      )
+      const { data } = await userLoginRequest(email, password)
       // store user's token in local storage
       rememberMe ? localStorage.setItem('userToken', data.body.token) : localStorage.removeItem('userToken')
       return data
--- src/service/authService.js
+++ src/service/authService.js
@@ -1,11 +1,44 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import axios from 'axios'
+
+const backendURL = 'http://localhost:3001/api/v1'
+
+export const userLoginRequest = async (email, password) => {
+  return await axios.post(
+    `${backendURL}/user/login`,
+    { email, password },
+    //config
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+}
+
+export const userUpdateRequest = async (firstName, lastName, token) => {
+   return await axios.put(
+      `${backendURL}/user/profile`,
+      {
+        "firstName": firstName,
+        "lastName": lastName
+      },
+      //config
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+}
 
 export const authApi = createApi({
     
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
     // base url of backend API
-    baseUrl: 'http://localhost:3001/api/v1',
+    baseUrl: backendURL,
     // prepareHeaders is used to configure the header of every request and gives access to getState which we use to include the token from the store
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.userToken
